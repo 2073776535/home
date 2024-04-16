d2b52bc6883056a5bb44b4436344ae17b77af1c5
2073776535 <74645368+2073776535@users.noreply.github.com>
2024-04-16 23:55:07 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -23,6 +23,7 @@
           &amp;
           {{ siteIcp }}
         </a>
+        <img src="./images/beian.png" style="width:20px;height:20px"/>
         <a href="https://beian.mps.gov.cn/#/query/webSearch?code=41090002410931" rel="noreferrer" target="_blank">豫公网安备41090002410931</a>
       </div>
       <div v-else class="lrc">
